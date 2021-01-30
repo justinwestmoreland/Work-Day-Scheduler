7a1e377d7773f6cbb647c1ec6ace9ee8d3a03ac0
Justin Westmoreland <justinrwestmoreland@gmail.com>
2021-01-30 12:12:01 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// set the time and the date at the top of the page when the page opens. 
+// time blocks should change color when either past, present, or future
+// this will require the need to grab the current hour or current time
+// this will also need to check each time block on the page to see if it's past, present, or fuuture
+
+
+// user will input information onto the calendar and this info will need to be saved locally and printed to the screen. 
+// make sure the data saves when the save button is hit
+// set row text based on what has been saved into local storage by the user
